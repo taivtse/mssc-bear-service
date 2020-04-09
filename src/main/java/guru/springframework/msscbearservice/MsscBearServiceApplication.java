cda7857ac4f513dd949ccdc8c9e441c786cd2329
taivo <taivtse@gmail.com>
2020-04-09 21:35:30 +0700

    40. LC Beer Service - Initial Spring Boot Project Creation

---
+++ src/main/java/guru/springframework/msscbearservice/MsscBearServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbearservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBearServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBearServiceApplication.class, args);
+    }
+
+}
